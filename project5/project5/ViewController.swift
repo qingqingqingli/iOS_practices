ff2c46e244194ce3ae92cfcc9ab770da2d14b83e
qli <kadlqbetty@gmail.com>
2022-04-27 14:47:58 +0200

    add error and do catch block

--- project5/project5/ViewController.swift
+++ project5/project5/ViewController.swift
@@ -8,12 +8,14 @@
 import UIKit
 
 class ViewController: UITableViewController {
-
+    
     var allWords = [String]()
     var usedWords = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Word")
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAnswer))
         
@@ -42,7 +44,7 @@ class ViewController: UITableViewController {
         let ac = UIAlertController(title: "Enter answer", message: nil, preferredStyle: .alert)
         ac.addTextField()
         
-        let sumbitAction = UIAlertAction(title: "Submit", style: .default) { [weak self, weak ac] action in
+        let sumbitAction = UIAlertAction(title: "Submit", style: .default) { [weak self, weak ac] _ in
             guard let answer = ac?.textFields?[0].text else { return }
             self?.submit(answer)
         }
@@ -52,8 +54,100 @@ class ViewController: UITableViewController {
     }
     
     private func submit(_ answer: String) {
+        let lowerAnswer = answer.lowercased()
+        let errorTitle: String
+        let errorMessage: String
         
+        func showErrorAlert() {
+            let ac = UIAlertController(title: errorTitle, message: errorMessage, preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+        }
+        
+        do {
+            guard try isNotEmpty(word: lowerAnswer),
+                  try isPossible(word: lowerAnswer),
+                  try isOriginal(word: lowerAnswer),
+                  try isReal(word: lowerAnswer) else { return }
+            
+            usedWords.insert(answer, at: 0)
+            
+            let indexPath = IndexPath(row: 0, section: 0)
+            tableView.insertRows(at: [indexPath], with: .automatic)
+            
+        } catch WordCheckError.isEmpty {
+            errorTitle = "Empty word"
+            errorMessage = "You can't provide an empty word."
+            showErrorAlert()
+        }
+        catch WordCheckError.isNotPossible{
+            errorTitle = "Word not recognised"
+            errorMessage = "You can't just make them up, you know!"
+            showErrorAlert()
+        }
+        catch WordCheckError.isNotOriginal {
+            errorTitle = "Word used already"
+            errorMessage = "Be more original!"
+            showErrorAlert()
+        }
+        catch WordCheckError.isNotReal {
+            guard let title = title?.lowercased() else { return }
+            errorTitle = "Word not possible"
+            errorMessage = "You can't spell that word from \(title)"
+            showErrorAlert()
+        }
+        catch {
+            print("\(error): Not specified. ")
+        }
     }
+    
+    private func isNotEmpty(word: String) throws -> Bool {
+        if !word.isEmpty { return true }
+        
+        throw WordCheckError.isEmpty
+    }
+    
+    private func isPossible(word: String) throws -> Bool {
+        var tmpWord = title?.lowercased()
+        
+        for letter in word {
+            if let index = tmpWord?.firstIndex(of: letter) {
+                tmpWord?.remove(at: index)
+            } else {
+                throw WordCheckError.isNotPossible
+            }
+        }
+        return true
+    }
+    
+    private func isOriginal(word: String) throws -> Bool {
+        guard usedWords.contains(word) == false else {
+            throw WordCheckError.isNotOriginal
+        }
+        return true
+    }
+    
+    private func isReal(word: String) throws -> Bool {
+        let checker = UITextChecker()
+        
+        // Important to convert to utf16 strings when working with UIKit
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        
+        if misspelledRange.location == NSNotFound {
+            return true
+        } else {
+            throw WordCheckError.isNotReal
+        }
+    }
+    
+    enum WordCheckError: Error {
+        case isEmpty
+        case isNotPossible
+        case isNotOriginal
+        case isNotReal
+    }
+    
 }
 
 extension ViewController {
@@ -66,6 +160,6 @@ extension ViewController {
         cell.textLabel?.text = usedWords[indexPath.row]
         return cell
     }
-    
 }
+
 
