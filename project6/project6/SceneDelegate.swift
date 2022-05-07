2fe4d6e6ddf7d23964fcae07fcc03691b33d37c1
qli <kadlqbetty@gmail.com>
2022-05-07 11:43:20 +0200

    add project 6

---
+++ project6/project6/SceneDelegate.swift
@@ -0,0 +1,36 @@
+//
+//  Created by Qing Li on 07/05/2022.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        if let windowScene = scene as? UIWindowScene {
+            let window = UIWindow(windowScene: windowScene)
+            let rootVC = ViewController()
+            window.rootViewController = UINavigationController(rootViewController: rootVC)
+            self.window = window
+            window.makeKeyAndVisible()
+        }
+    }
+    
+    func sceneDidDisconnect(_ scene: UIScene) {
+    }
+    
+    func sceneDidBecomeActive(_ scene: UIScene) {
+    }
+    
+    func sceneWillResignActive(_ scene: UIScene) {
+    }
+    
+    func sceneWillEnterForeground(_ scene: UIScene) {
+    }
+    
+    func sceneDidEnterBackground(_ scene: UIScene) {
+    }
+}
+
